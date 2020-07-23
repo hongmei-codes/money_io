af81a239c35c41ca969ad09cfe9569d0d3ef8f68
hongmei <duhongmei@protonmail.com>
2020-07-23 17:47:09 +0800

    Updates

---
+++ precache-manifest.b6cdc175e6f2456fe7397076909d80d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c582f8649a8f05108bc8eb81f9ca68ed",
+    "url": "/money_io/index.html"
+  },
+  {
+    "revision": "1f1385f1802451f9fb29",
+    "url": "/money_io/static/css/main.4d5d8e7a.chunk.css"
+  },
+  {
+    "revision": "fc2b3941d3aa64e2c756",
+    "url": "/money_io/static/js/2.27db1fb4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/money_io/static/js/2.27db1fb4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f1385f1802451f9fb29",
+    "url": "/money_io/static/js/main.d052c5aa.chunk.js"
+  },
+  {
+    "revision": "62719fc76b72dc36b824",
+    "url": "/money_io/static/js/runtime-main.cf31c5cf.js"
+  }
+]);
